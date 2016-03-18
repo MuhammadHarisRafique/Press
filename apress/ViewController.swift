4987ad03aa49c134a6f7750d92aa4b61f73a9b69
Mac <Mac@Macs-Mac.local>
2016-03-18 05:12:01 -0700

    push on githup

--- apress/ViewController.swift
+++ apress/ViewController.swift
@@ -10,16 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MySwitch: UISwitch!
+    @IBOutlet weak var btnsummit: UIButton!
+    @IBOutlet weak var btnButton: UIButton!
+    @IBOutlet weak var BtnSwitch: UIButton!
+    @IBOutlet weak var MySlider: UISlider!
+    @IBOutlet weak var PasswordTextBox: UITextField!
+    @IBOutlet weak var NameTextBox: UITextField!
+    @IBOutlet weak var MyImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        MyImageView.layer.cornerRadius = MyImageView.frame.size.width / 2
+        MyImageView.clipsToBounds = true
+        MyImageView.layer.borderColor = UIColor.blackColor().CGColor
+        MyImageView.layer.borderWidth = 2
+         MyImageView.layer.cornerRadius = MyImageView.frame.size.height / 2
+        
+            }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func txtName(sender: AnyObject) {
+        
+    }
+    @IBAction func txtName2(sender: AnyObject) {
+        if NameTextBox.text == "" {
+            
+            let alert = UIAlertController(title: "Alert", message: "Message", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+    
+    }
 }
-
+}
